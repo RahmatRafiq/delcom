a5796ec9779ee8affc7631ae9a8d1232323071e5
Rahmat Rafiq <rahmatrafiq.1999@gmail.com>
2025-12-15 00:10:52 +0700

    feat(config): add social platform API credentials config
    - Add Instagram, Twitter/X, Threads OAuth configuration
    - Add placeholder credentials to .env.example
    - Document API requirements (business account, paid tier)

--- config/services.php
+++ config/services.php
@@ -46,4 +46,55 @@ return [
         'redirect' => env('GITHUB_REDIRECT_URI'),
     ],
 
+    /*
+    |--------------------------------------------------------------------------
+    | Social Media Platform APIs
+    |--------------------------------------------------------------------------
+    */
+
+    // YouTube API scopes (used with Google OAuth)
+    'youtube' => [
+        'scopes' => [
+            'https://www.googleapis.com/auth/youtube.force-ssl',
+            'https://www.googleapis.com/auth/youtube.readonly',
+        ],
+    ],
+
+    // Instagram (Meta/Facebook Graph API)
+    // Requires Instagram Business/Creator account linked to Facebook Page
+    'instagram' => [
+        'client_id' => env('INSTAGRAM_CLIENT_ID'),
+        'client_secret' => env('INSTAGRAM_CLIENT_SECRET'),
+        'redirect' => env('INSTAGRAM_REDIRECT_URI'),
+        'scopes' => [
+            'instagram_basic',
+            'instagram_manage_comments',
+        ],
+    ],
+
+    // Twitter/X (OAuth 2.0)
+    // Requires Basic API tier ($100/mo) or higher
+    'twitter' => [
+        'client_id' => env('TWITTER_CLIENT_ID'),
+        'client_secret' => env('TWITTER_CLIENT_SECRET'),
+        'redirect' => env('TWITTER_REDIRECT_URI'),
+        'scopes' => [
+            'tweet.read',
+            'users.read',
+            'offline.access',
+        ],
+    ],
+
+    // Threads (Meta Graph API)
+    // Requires Instagram Business/Creator account
+    'threads' => [
+        'client_id' => env('THREADS_CLIENT_ID'),
+        'client_secret' => env('THREADS_CLIENT_SECRET'),
+        'redirect' => env('THREADS_REDIRECT_URI'),
+        'scopes' => [
+            'threads_basic',
+            'threads_manage_replies',
+        ],
+    ],
+
 ];
